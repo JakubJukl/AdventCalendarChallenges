7fdaaa3cfe719a68d707abbf835b991033e0e9c9
Jakub Jukl <jukljakub@gmail.com>
2022-12-01 15:20:33 +0100

    first challenge second edition

--- 01_calories/01_calories.js
+++ 01_calories/01_calories.js
@@ -16,5 +16,29 @@ function findMostCaloricElf(inputRows) {
     return mostCaloric;
 }
 
-const input = fs.readFileSync('input.txt', 'utf8');
-console.log(findMostCaloricElf(input.split('\r\n')));
+function findTopThreeCaloricElfs(inputRows) {
+    const topThree = [0, 0, 0];
+    let currentElf = 0;
+    for (let row of inputRows) {
+        if (row.length === 0) {
+            if (currentElf > topThree[0]) {
+                topThree[2] = topThree[1];
+                topThree[1] = topThree[0];
+                topThree[0] = currentElf;
+            } else if (currentElf > topThree[1]) {
+                topThree[2] = topThree[1];
+                topThree[1] = currentElf;
+            } else if (currentElf > topThree[2]) {
+                topThree[2] = currentElf;
+            }
+            currentElf = 0;
+        } else {
+            currentElf += parseInt(row);
+        }
+    }
+    return topThree[0] + topThree[1] + topThree[2];
+}
+
+const input = fs.readFileSync('input.txt', 'utf8').split('\r\n');
+console.log(findMostCaloricElf(input));
+console.log(findTopThreeCaloricElfs(input));
