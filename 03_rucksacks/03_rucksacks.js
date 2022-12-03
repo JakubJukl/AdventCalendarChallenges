585c0abb8c4794a1f81ece7b09ddea4cd884634e
Jakub Jukl <jukljakub@gmail.com>
2022-12-03 11:40:01 +0100

    first third challenge

---
+++ 03_rucksacks/03_rucksacks.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+
+function splitToCompartments(rucksack) {
+    if (rucksack.length % 2 === 0) {
+        const compartments = [];
+        compartments.push(rucksack.substring(0, rucksack.length/2));
+        compartments.push(rucksack.substring(rucksack.length/2, rucksack.length));
+        return compartments;
+    } else throw new Error('Rucksack length is not even');
+}
+
+function findDuplicateSupplies(compartments) {
+    const duplicateSupplies = [];
+    const rucksackDict = {}
+    for (let supply of compartments[0]) {
+        rucksackDict[supply] = 1;
+    }
+    for (let supply of compartments[1]) {
+        if (supply in rucksackDict && !duplicateSupplies.includes(supply)) {
+            duplicateSupplies.push(supply);
+        }
+    }
+    return duplicateSupplies;
+}
+
+function getPriorityValue(supply) {
+    const charCode = supply.charCodeAt(0);
+    let priority;
+    // lowercase and uppercase have different priorities
+    if (charCode >= 97) priority = charCode - 96;
+    else priority = charCode - 38;
+    return priority;
+}
+
+function sumPriorityValues(duplicateSupplies) {
+    let sum = 0;
+    for (let supply of duplicateSupplies) {
+        sum += getPriorityValue(supply);
+    }
+    return sum;
+}
+
+function sumRucksacksPriorities(rucksacks) {
+    let sum = 0;
+    for (let rucksack of rucksacks) {
+        const compartments = splitToCompartments(rucksack);
+        const duplicates = findDuplicateSupplies(compartments);
+        sum += sumPriorityValues(duplicates);
+    }
+    return sum;
+}
+
+const input = fs.readFileSync('input.txt', 'utf8').split('\r\n');
+console.log(sumRucksacksPriorities(input));
