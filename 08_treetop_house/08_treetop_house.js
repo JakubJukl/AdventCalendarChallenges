87b52eb5ea283dfe5c0051f7dd34387a295329e3
Jakub Jukl <jukljakub@gmail.com>
2022-12-08 09:46:22 +0100

    eight challenge, part two

--- 08_treetop_house/08_treetop_house.js
+++ 08_treetop_house/08_treetop_house.js
@@ -11,6 +11,27 @@ function checkCols(inputRows, row, column, start, end, theTreeHeight) {
     return visible;
 }
 
+function countVisibleCols(inputRows, row, column, start, end, theTreeHeight) {
+    let visibleCount = 0;
+    const increment = start < end;
+    // it's more readable with two for loops, in an if else statement, I am well aware
+    // it can be done without it, but I don't pay for lines of code, so I don't care
+    if (increment) {
+        for (let i = start; i < end; i++) {
+            const currentTreeHeight = parseInt(inputRows[i][column]);
+            visibleCount += 1;
+            if (currentTreeHeight >= theTreeHeight) break;
+        }
+    } else {
+        for (let i = start; i > end; i--) {
+            const currentTreeHeight = parseInt(inputRows[i][column]);
+            visibleCount += 1;
+            if (currentTreeHeight >= theTreeHeight) break;
+        }
+    }
+    return visibleCount;
+}
+
 function checkRows(inputRows, row, column, start, end, theTreeHeight) {
     let visible = true;
     for (let i = start; i < end; i++) {
@@ -21,6 +42,26 @@ function checkRows(inputRows, row, column, start, end, theTreeHeight) {
     }
     return visible;
 }
+
+function countVisibleRows(inputRows, row, column, start, end, theTreeHeight) {
+    let visibleCount = 0;
+    const increment = start < end;
+    if (increment) {
+        for (let i = start; i < end; i++) {
+            const currentTreeHeight = parseInt(inputRows[row][i]);
+            visibleCount += 1;
+            if (currentTreeHeight >= theTreeHeight) break;
+        }
+    } else {
+        for (let i = start; i > end; i--) {
+            const currentTreeHeight = parseInt(inputRows[row][i]);
+            visibleCount += 1;
+            if (currentTreeHeight >= theTreeHeight) break;
+        }
+    }
+    return visibleCount;
+}
+
 
 function isTreeVisible(inputRows, row, column) {
     const theTreeHeight = parseInt(inputRows[row][column]);
@@ -48,5 +89,25 @@ function countVisibleTrees(inputRows) {
     return sum;
 }
 
+function calcScenicScore(inputRows, row, column) {
+    return countVisibleCols(inputRows, row, column, row - 1, -1, parseInt(inputRows[row][column])) *
+    countVisibleCols(inputRows, row, column, row + 1, inputRows.length, parseInt(inputRows[row][column])) *
+    countVisibleRows(inputRows, row, column, column - 1, -1, parseInt(inputRows[row][column])) *
+    countVisibleRows(inputRows, row, column, column + 1, inputRows[row].length, parseInt(inputRows[row][column]));
+}
+
+function getTreeScenicScore(inputRows) {
+    if (inputRows[inputRows.length - 1].length === 0) inputRows.pop();
+    let highestScore = 0;
+    for (let i = 0; i < inputRows.length; i++) {
+        for (let j = 0; j < inputRows[i].length; j++) {
+            const tempScore = calcScenicScore(inputRows, i, j);
+            if (tempScore > highestScore) highestScore = tempScore;
+        }
+    }
+    return highestScore;
+}
+
 const input = fs.readFileSync('input.txt', 'utf8').split('\r\n');
 console.log(countVisibleTrees(input));
+console.log(getTreeScenicScore(input));
